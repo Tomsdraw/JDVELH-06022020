2d774d494608efb21ee7f05d53db78bb7bbf0038
Tomsdraw <th.pezzoli@gmail.com>
2020-02-06 10:49:07 +0100

    Gestion des lieux

--- jdvelh.c
+++ jdvelh.c
@@ -3,30 +3,49 @@
 #define TAILLE 20
 
 
-struct Lieu {
+struct Chapitre {
     char name[20];
+    char description[40];
+    int gold;
+    int pv;
+    int id[20];
  };
 
- typedef struct Lieu lieu;
+ typedef struct Chapitre chapitre;
+
+ int i;
+ int userInput;
+ chapitre destination[TAILLE] = {{"Chapitre 1","Le chateau imperial",20,70,{2,3}},{"Chapitre 2","La taverne des orcs",70,20,{1,3}},{"Chapitre 3","Village envahi",45,40,{1,2}}};
+
+int deplacement(){
+
+  for (i = 0; i < 2; i++){
+    printf("Chapitre %d\n", destination[userInput].id[i]);
+  }
+  printf("Entrez le numero du chapitre que vous souhaitez rejoindre\n");
+    scanf("%d",&userInput);
+  userInput=userInput-1;
+  printf("\nLa destination %s est atteinte.\n%s \n +%d gold +%d PV\n",destination[userInput].name,destination[userInput].description,destination[userInput].gold,destination[userInput].pv );
+  }
+
 
 int main(){
-  int i;
-  int userInput;
-
-lieu destination[TAILLE] = {{"Plaine"},{"Chateau"}};
-
-printf("Destinations disponibles :\n");
-for (i = 0; i < 2; i++){
-    printf("%d - %s\n", i, destination[i].name);
+printf("Chapitres disponibles :\n");
+for (i = 0; i < 3; i++){
+    printf("%s\n",destination[i].name);
 }
-
+printf("Entrez le numero du chapitre que vous souhaitez rejoindre\n");
 scanf("%d",&userInput );
 
-while((-1 > userInput || userInput > 1)){
+while((0> userInput || userInput > 3)){
   printf("Ce choix n'est pas valide\n" );
   scanf("%d",&userInput );
 }
 
-printf("La destination %s est atteinte.\n",destination[userInput].name );
+userInput=userInput-1;
+
+printf("\n%s atteint.\n%s \n +%d gold +%d PV\n\n",destination[userInput].name,destination[userInput].description,destination[userInput].gold,destination[userInput].pv );
+
+deplacement();
 
 }
